4139c6fc5e58297f073516fffea5c737cb63868d
irenebartolini02 <s345905@studenti.polito.it>
2025-11-04 19:27:02 +0100

    funziona

--- data/data.py
+++ data/data.py
@@ -5,14 +5,14 @@ from torchvision.datasets import ImageFolder
 import torchvision.transforms as T
 
 
-with open('tiny-imagenet/tiny-imagenet-200/val/val_annotations.txt') as f:
+with open('dataset/tiny-imagenet/tiny-imagenet-200/val/val_annotations.txt') as f:
     for line in f:
         fn, cls, *_ = line.split('\t')
-        os.makedirs(f'tiny-imagenet/tiny-imagenet-200/val/{cls}', exist_ok=True)
+        os.makedirs(f'dataset/tiny-imagenet/tiny-imagenet-200/val/{cls}', exist_ok=True)
 
-        shutil.copyfile(f'tiny-imagenet/tiny-imagenet-200/val/images/{fn}', f'tiny-imagenet/tiny-imagenet-200/val/{cls}/{fn}')
+        shutil.copyfile(f'dataset/tiny-imagenet/tiny-imagenet-200/val/images/{fn}', f'dataset/tiny-imagenet/tiny-imagenet-200/val/{cls}/{fn}')
 
-shutil.rmtree('tiny-imagenet/tiny-imagenet-200/val/images')
+shutil.rmtree('dataset/tiny-imagenet/tiny-imagenet-200/val/images')
 
 
 transform = T.Compose([
@@ -23,8 +23,8 @@ transform = T.Compose([
 
 # root/{classX}/x001.jpg
 
-tiny_imagenet_dataset_train = ImageFolder(root='tiny-imagenet/tiny-imagenet-200/train', transform=transform)
-tiny_imagenet_dataset_val = ImageFolder(root='tiny-imagenet/tiny-imagenet-200/val', transform=transform)
+tiny_imagenet_dataset_train = ImageFolder(root='dataset/tiny-imagenet/tiny-imagenet-200/train', transform=transform)
+tiny_imagenet_dataset_val = ImageFolder(root='dataset/tiny-imagenet/tiny-imagenet-200/val', transform=transform)
 
 train_loader = torch.utils.data.DataLoader(tiny_imagenet_dataset_train, batch_size=32, shuffle=True, num_workers=8)
 val_loader = torch.utils.data.DataLoader(tiny_imagenet_dataset_val, batch_size=32, shuffle=False)
